67d288a1ca9befa3056285c785cd5b941f8eb9b1
malteish <malteish@corpus.io>
2023-05-23 13:49:45 +0200

    add input

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [secretMessage, setSecretMessage] = useState<string>("Hello World");
+  const [message, setMessage] = useState<string>("Hello World");
+
+  function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
+    event.preventDefault();
+    setSecretMessage(event.currentTarget.value);
+  }
+
+  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    console.log(secretMessage);
+    setMessage(secretMessage);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <form onSubmit={handleSubmit}>
+          <label>
+            Secret 1
+            <input
+              id="message"
+              value={secretMessage}
+              type="text"
+              onChange={(event) => handleChange(event)}
+            />
+            <button type="submit">Submit</button>
+          </label>
+        </form>
+        <p>Current secret: {message}</p>
       </header>
     </div>
   );
