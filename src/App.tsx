6066bf6c5cc7187ef02239751df6550e8ae8f55e
malteish <malteish@corpus.io>
2023-05-31 10:29:55 +0200

    add chakra

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
 import { useEffect } from "react";
 import { useState } from "react";
+import {
+  Box,
+  Heading,
+  Button,
+  UnorderedList,
+  ListItem,
+  Text,
+} from "@chakra-ui/react";
+import { Collapse } from "@chakra-ui/transition";
 import "./App.css";
 import BN from "bn.js";
 import { lagrangeInterpolation } from "./recover";
@@ -12,6 +21,8 @@ function App() {
   const [indexes, setIndexes] = useState<BN[]>([]);
   const [privateKey, setPrivateKey] = useState<BN>();
   const [ethAddress, setEthAddress] = useState("");
+  const [show, setShow] = useState(false);
+  const handleToggle = () => setShow(!show);
 
   function handleSubmit(event: any) {
     event.preventDefault();
@@ -33,40 +44,16 @@ function App() {
     setPrivateKey(undefined);
   }
 
+  /**
+   * Reconstructs private key from shares and indexes
+   * @param shares y-coordinates of the points on the polynomial
+   * @param indexes x-coordinates of the points on the polynomial
+   * @param setPrivateKey callback to set the private key
+   */
   function getPrivateKey(shares: BN[], indexes: BN[], setPrivateKey: any) {
-    // let shares: BN[] = [];
-    // let indexes: BN[] = [];
-
-    // secrets for
-    // polynomialID:"020a4557aca651b4078e9a73ecd8af3ed87ca057da31dd91e26f9c9e8262ed43da|02663c58290b1862534ff2a3dd2297a6da4d82cba176908ce5f5973922182d1751"
-    // extracted from browser
-    // const deviceShareSecret =
-    //   "b6dac735ebfa40ef58031382071ae60c738ac693e0f2c542e3b4f5e7d9e367a4";
-    // const deviceShareIndex =
-    //   "6def503eea5d321c285abdcd385c0451f2b6acd0d05c522c7e3c032e0de9fb4b";
-
-    // const providerShareSecret =
-    //   "dd819ae466a9a6e3e297aa629caa405af31f7806c8a48677ed6c55c0c8e64e34";
-    // const providerShareIndex = "1";
-
-    // const privateKey = new BN(
-    //   "dc146f1a3f4ae942d14ce82676c30b8cc933f0149e807e802b6f2039452083a9",
-    //   "hex"
-    // );
-
-    // shares.push(new BN(deviceShareSecret, "hex"));
-    // indexes.push(new BN(deviceShareIndex, "hex"));
-    // shares.push(new BN(providerShareSecret, "hex"));
-    // indexes.push(new BN(providerShareIndex, "hex"));
-
     const calculatedPrivateKey = lagrangeInterpolation(shares, indexes);
     setPrivateKey(calculatedPrivateKey);
     console.log("calculatedPrivateKey: ", calculatedPrivateKey.toString("hex"));
-    // if (calculatedPrivateKey.eq(privateKey)) {
-    //   console.log("Found the private key: ", privateKey.toString("hex"));
-    // } else {
-    //   console.log("Private key not found");
-    // }
   }
 
   useEffect(() => {
@@ -84,69 +71,77 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div>
-          <h2>Shamir's Secret Sharing Scheme recovery</h2>
+        <Box>
+          <Heading>Shamir's Secret Sharing Scheme recovery</Heading>
           <h3>
             {" "}
             as implemented by <a href="https://web3auth.io/tech">web3auth.io</a>
           </h3>
-          <p>
+          <Text>
             Input the number of shares needed for recovery, along with their
             index.{" "}
-          </p>
-          <ul>
-            <li>
+          </Text>
+          <UnorderedList>
+            <ListItem>
               share refers to the y coordinate of the point on the polynomial
-            </li>
-            <li>
+            </ListItem>
+            <ListItem>
               index refers to the x coordinate of the point on the polynomial
-            </li>
-            <ul>
-              <li>index of the device social recovery share is always 1</li>
-              <li>index of the other shares are random and too big to guess</li>
-            </ul>
-            <li>
+            </ListItem>
+            <UnorderedList>
+              <ListItem>
+                index of the device social recovery share is always 1
+              </ListItem>
+              <ListItem>
+                index of the other shares are random and too big to guess
+              </ListItem>
+            </UnorderedList>
+            <ListItem>
               device share and index can be extracted from browser storage
-            </li>
-            <li>
+            </ListItem>
+            <ListItem>
               using shares that do not belong to the same polynomial will not
               fail, but definitely yield a wrong result
-            </li>
-            <li>working example</li>
-            <ul>
-              <li>
-                deviceShare
-                <ul>
-                  <li>
-                    share:
-                    "b6dac735ebfa40ef58031382071ae60c738ac693e0f2c542e3b4f5e7d9e367a4"
-                  </li>
-                  <li>
-                    index:
-                    "6def503eea5d321c285abdcd385c0451f2b6acd0d05c522c7e3c032e0de9fb4b"
-                  </li>
-                </ul>
-              </li>
-              <li>
-                providerShare
-                <ul>
-                  <li>
-                    share:
-                    "dd819ae466a9a6e3e297aa629caa405af31f7806c8a48677ed6c55c0c8e64e34"
-                  </li>
-                  <li>index: "1"</li>
-                </ul>
-              </li>
-              <li>
-                privateKey:
-                "dc146f1a3f4ae942d14ce82676c30b8cc933f0149e807e802b6f2039452083a9"
-              </li>
-              <li>
-                Ethereum Address: 0x97dA24fF2a92C94Db0535cf8D20FcA0Ab6dB9876
-              </li>
-            </ul>
-          </ul>
-        </div>
+            </ListItem>
+            <ListItem>
+              <Button onClick={handleToggle}>show example</Button>
+            </ListItem>
+            <Collapse in={show}>
+              <UnorderedList>
+                <ListItem>
+                  deviceShare
+                  <UnorderedList>
+                    <ListItem>
+                      share:
+                      "b6dac735ebfa40ef58031382071ae60c738ac693e0f2c542e3b4f5e7d9e367a4"
+                    </ListItem>
+                    <ListItem>
+                      index:
+                      "6def503eea5d321c285abdcd385c0451f2b6acd0d05c522c7e3c032e0de9fb4b"
+                    </ListItem>
+                  </UnorderedList>
+                </ListItem>
+                <ListItem>
+                  providerShare
+                  <UnorderedList>
+                    <ListItem>
+                      share:
+                      "dd819ae466a9a6e3e297aa629caa405af31f7806c8a48677ed6c55c0c8e64e34"
+                    </ListItem>
+                    <ListItem>index: "1"</ListItem>
+                  </UnorderedList>
+                </ListItem>
+                <ListItem>
+                  privateKey:
+                  "dc146f1a3f4ae942d14ce82676c30b8cc933f0149e807e802b6f2039452083a9"
+                </ListItem>
+                <ListItem>
+                  Ethereum Address: 0x97dA24fF2a92C94Db0535cf8D20FcA0Ab6dB9876
+                </ListItem>
+              </UnorderedList>
+            </Collapse>
+          </UnorderedList>
+        </Box>
         <form onSubmit={handleSubmit}>
           <p>
             <label>
